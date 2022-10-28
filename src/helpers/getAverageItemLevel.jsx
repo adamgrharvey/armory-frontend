6f000898b63f4f907aae0b5a32b8fa11b56d8111
adamgrharvey <adamgrharvey@gmail.com>
2022-10-27 19:06:17 -0700

    fixes math on avg ilvl for 2hand weapons

--- src/helpers/getAverageItemLevel.jsx
+++ src/helpers/getAverageItemLevel.jsx
@@ -3,12 +3,12 @@ export default function getAverageItemLevel(character) {
   let keys = Object.keys(character);
   let sum = 0;
   let itemCount = 0;
-  console.log(keys);
-  console.log(character);
 
   for (const key of keys) {
     if (key !== '3' && key !== '18') {
-      
+      if (key === '15' && character[key].inventory_type.name === 'Two-Hand') {
+        sum += character[key].level;
+      }
       sum += character[key].level;
       itemCount++;
     }
