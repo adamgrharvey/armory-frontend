afbc2432f9aafd9a7dc74a423bae156f62618d12
adamgrharvey <adamgrharvey@gmail.com>
2022-10-27 18:37:04 -0700

    classID to className helper

---
+++ src/helpers/classIDtoName.jsx
@@ -0,0 +1,6 @@
+export default function classIDtoName(id) {
+
+  let classArr = ["WARRIOR", "PALADIN", "HUNTER", "ROGUE", "PRIEST", "DEATHKNIGHT", "SHAMAN", "MAGE", "WARLOCK", "MONK", "DRUID", "DEMONHUNTER", "EVOKER"];
+
+  return classArr[id+1];
+}
