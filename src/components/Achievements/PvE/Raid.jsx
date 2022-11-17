da198ec0d6a841b230a3a445a42ff11785e1d07d
adamgrharvey <adamgrharvey@gmail.com>
2022-11-17 15:38:39 -0800

    styling and state for Raid component

--- src/components/Achievements/PvE/Raid.jsx
+++ src/components/Achievements/PvE/Raid.jsx
@@ -1,4 +1,9 @@
+import { useState, useEffect } from "react";
+
 export default function Raid(props) {
+
+  const [show1, setShow1] = useState(false);
+  const [show2, setShow2] = useState(false);
 
   const getRaidProgress = function (stats, difficulty, instance) {
     let count = 0;
@@ -18,6 +23,7 @@ export default function Raid(props) {
 
     return [count, instance.bosses.length, progressLevel];
   }
+
   let [progress10, bossCount10, progressLevel10] = getRaidProgress(props.stats, "10bossIDs", props.instance);
   let [progress25, bossCount25, progressLevel25] = getRaidProgress(props.stats, "25bossIDs", props.instance);
 
@@ -33,26 +39,51 @@ export default function Raid(props) {
         </div>
       </div>
       <div className="instanceprogress">
-        <div className="Progressbar Progressbar--leftText Progressbar--levelColor Progressbar--fraction" data-queryselectoralways-ignore="true">
-          <div className="Progressbar-progress" data-progresslevel={progressLevel10} style={{ width: `${progress10 / bossCount10 * 100}%` }}></div>
-          <div class="Progressbar-content"><div class="Progressbar-fraction">{`${progress10} / ${bossCount10}`}</div></div>
-        </div>
-        <div className="Progressbar Progressbar--leftText Progressbar--levelColor Progressbar--fraction" data-queryselectoralways-ignore="true">
-          <div className="Progressbar-progress" data-progresslevel={progressLevel25} style={{ width: `${progress25 / bossCount25 * 100}%` }}></div>
-          <div class="Progressbar-content"><div class="Progressbar-fraction">{`${progress25} / ${bossCount25}`}</div></div>
-        </div>
-
-
-        {props.instance.bosses.map((i, index) => {
-          return (
+        <div className="10man">
+          <div>
             <div>
-              {`${i} ${props.stats[props.instance["25bossIDs"][index]].value}`}
+              10man
             </div>
-          )
+            <div onClick={() => {
+              setShow1(prev => !prev)
+            }} className="Progressbar Progressbar--leftText Progressbar--levelColor Progressbar--fraction" data-queryselectoralways-ignore="true">
+              <div className="Progressbar-progress" data-progresslevel={progressLevel10} style={{ width: `${progress10 / bossCount10 * 100}%` }}></div>
+              <div class="Progressbar-content"><div class="Progressbar-fraction">{`${progress10} / ${bossCount10}`}</div></div>
+            </div>
+          </div>
+          <div className={`statistics${show1 ? "" : " none"}`}>
+            {props.instance.bosses.map((i, index) => {
+              return (
+                <div>
+                  {`${i} x ${props.stats[props.instance["10bossIDs"][index]].value}`}
+                </div>
+              )
+            })}
+          </div>
 
-
-
-        })}
+        </div>
+        <div className="25man">
+          <div>
+            <div>
+              25man
+            </div>
+            <div onClick={() => {
+              setShow2(prev => !prev)
+            }} className="Progressbar Progressbar--leftText Progressbar--levelColor Progressbar--fraction" data-queryselectoralways-ignore="true">
+              <div className="Progressbar-progress" data-progresslevel={progressLevel25} style={{ width: `${progress25 / bossCount25 * 100}%` }}></div>
+              <div class="Progressbar-content"><div class="Progressbar-fraction">{`${progress25} / ${bossCount25}`}</div></div>
+            </div>
+          </div>
+          <div className={`statistics${show2 ? "" : " none"}`}>
+            {props.instance.bosses.map((i, index) => {
+              return (
+                <div>
+                  {`${i} x ${props.stats[props.instance["25bossIDs"][index]].value}`}
+                </div>
+              )
+            })}
+          </div>
+        </div>
       </div>
     </div>
   )
