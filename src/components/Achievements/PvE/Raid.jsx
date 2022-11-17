acbac09b9df9b5a1bc64934e1b3d608bcc150130
adamgrharvey <adamgrharvey@gmail.com>
2022-11-17 01:23:40 -0800

    adds React-ProgressBar

--- src/components/Achievements/PvE/Raid.jsx
+++ src/components/Achievements/PvE/Raid.jsx
@@ -1,5 +1,16 @@
-
 export default function Raid(props) {
+
+  const getRaidProgress = function (stats, difficulty, instance) {
+    let count = 0;
+    for (let i = 0; i < instance.bosses.length; i++) {
+      if (stats[instance[difficulty][i]].value !== "0") {
+        count++;
+      }
+    }
+    return [count, instance.bosses.length];
+  }
+  let [progress10, bossCount10] = getRaidProgress(props.stats, "10bossIDs", props.instance);
+  let [progress25, bossCount25] = getRaidProgress(props.stats, "25bossIDs", props.instance);
 
   return (
     <div className="Raid">
@@ -13,6 +24,15 @@ export default function Raid(props) {
         </div>
       </div>
       <div className="instanceprogress">
+        <div>
+          {`10man: ${progress10} / ${bossCount10}`}
+          
+        </div>
+        <div>
+          {`25man: ${progress25} / ${bossCount25}`}
+        </div>
+
+
         {props.instance.bosses.map((i, index) => {
           return (
             <div>
