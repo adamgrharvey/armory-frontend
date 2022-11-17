3a15bde2bd09c4468ff190199568d040886a00d5
adamgrharvey <adamgrharvey@gmail.com>
2022-11-17 01:49:52 -0800

    progress bar working

--- src/components/Achievements/PvE/Raid.jsx
+++ src/components/Achievements/PvE/Raid.jsx
@@ -2,15 +2,24 @@ export default function Raid(props) {
 
   const getRaidProgress = function (stats, difficulty, instance) {
     let count = 0;
+    let progressLevel = "low"
     for (let i = 0; i < instance.bosses.length; i++) {
       if (stats[instance[difficulty][i]].value !== "0") {
         count++;
       }
     }
-    return [count, instance.bosses.length];
+
+    if (count / instance.bosses.length > 0.25) {
+      progressLevel = "medium";
+      if (count / instance.bosses.length > 0.8) {
+        progressLevel = "high";
+      }
+    }
+
+    return [count, instance.bosses.length, progressLevel];
   }
-  let [progress10, bossCount10] = getRaidProgress(props.stats, "10bossIDs", props.instance);
-  let [progress25, bossCount25] = getRaidProgress(props.stats, "25bossIDs", props.instance);
+  let [progress10, bossCount10, progressLevel10] = getRaidProgress(props.stats, "10bossIDs", props.instance);
+  let [progress25, bossCount25, progressLevel25] = getRaidProgress(props.stats, "25bossIDs", props.instance);
 
   return (
     <div className="Raid">
@@ -24,12 +33,13 @@ export default function Raid(props) {
         </div>
       </div>
       <div className="instanceprogress">
-        <div>
-          {`10man: ${progress10} / ${bossCount10}`}
-          
+        <div className="Progressbar Progressbar--leftText Progressbar--levelColor Progressbar--fraction" data-queryselectoralways-ignore="true">
+          <div className="Progressbar-progress" data-progresslevel={progressLevel10} style={{ width: `${progress10 / bossCount10 * 100}%` }}></div>
+          <div class="Progressbar-content"><div class="Progressbar-fraction">{`${progress10} / ${bossCount10}`}</div></div>
         </div>
-        <div>
-          {`25man: ${progress25} / ${bossCount25}`}
+        <div className="Progressbar Progressbar--leftText Progressbar--levelColor Progressbar--fraction" data-queryselectoralways-ignore="true">
+          <div className="Progressbar-progress" data-progresslevel={progressLevel25} style={{ width: `${progress25 / bossCount25 * 100}%` }}></div>
+          <div class="Progressbar-content"><div class="Progressbar-fraction">{`${progress25} / ${bossCount25}`}</div></div>
         </div>
 
 
