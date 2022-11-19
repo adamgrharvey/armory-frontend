6c6d63cad7e6725743bda0216a6fedb44032f5dc
adamgrharvey <adamgrharvey@gmail.com>
2022-11-18 18:49:11 -0800

    style update and fixes bug with talent trees not changing which is displayed

--- src/components/Talents.jsx
+++ src/components/Talents.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react"
 
 export default function Talents(props) {
   let none = "Talents";
-  if (props.display === 'none') {
+  if (props.display === "") {
     none = 'none-talents';
   }
 
