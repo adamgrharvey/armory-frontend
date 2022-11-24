0d977399432942f412c4f40cadb381dadcefb8e7
adamgrharvey <adamgrharvey@gmail.com>
2022-11-24 14:40:56 -0800

    PvP bracket styling

--- src/components/PvP/Bracket.jsx
+++ src/components/PvP/Bracket.jsx
@@ -10,14 +10,40 @@ export default function Bracket(props) {
   // unranked
   // no data
 
-  const arenaImage = function(ratingString) {
+  const getArenaImageData = function (ratingString) {
     let rating = Number(ratingString);
+
+    if (!rating) {
+      return "None"
+    }
+    if (rating < 1400) {
+      return { "css": "Unranked", label: "Unranked" }
+    }
+    if (rating < 1550) {
+      return { "css": "Challenger", label: "Challenger I" }
+    }
+    if (rating < 1775) {
+      return { "css": "Challenger", label: "Challenger II" }
+    }
+    if (rating < 1925) {
+      return { "css": "Rival", label: "Rival I" }
+    }
+    if (rating < 2075) {
+      return { "css": "Rival", label: "Rival II" }
+    }
+    if (rating < 2375) {
+      return { "css": "Duelist", label: "Duelist" }
+    }
+    return { "css": "Elite", label: "Elite" }
   }
 
+  let arenaImageData = getArenaImageData(props.PvP[props.label].rating);
+
   return (
-    <div className='HonorKills'>
-      <div className={`PVPLogo PVPLogo-HORDE`}/>
+    <div className='Bracket'>
+      <div className={`PVPLogo ArenaRank-${arenaImageData.css}`} />
       <div className='Title'>{`${props.label}`}</div>
+      <div className='Title'>{`${arenaImageData.label}`}</div>
       <div className='honorkillsdropdown'>
         <div>
           {`Current Rating: ${props.PvP[props.label].rating}`}
