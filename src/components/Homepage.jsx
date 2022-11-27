ff2ed20688eb33675b4400bbd3a34111f16838c7
adamgrharvey <adamgrharvey@gmail.com>
2022-11-26 23:04:11 -0800

    adds background images for Homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,17 +1,54 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
 export default function Homepage(props) {
 
+  let Backgrounds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const [BG, setBG] = useState(1);
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(true);
+
+  const nextBG = function (currentIndex) {
+    let max = Backgrounds.length;
+
+    if (currentIndex === max) {
+      setBG(1);
+    } else {
+      setBG(currentIndex + 1);
+    }
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (seconds === 20) {
+      setSeconds(0);
+      nextBG(BG);
+    }
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds + 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
   return (
-    <div className="Splash">
-      <div>
-        WELCOME
-      </div>
-      <div className="bla1">
-        TO
-      </div>
-      <div className="bla2">
-        CLASSIC ARMORY
+    <div className={`Homepage BG-${BG}`}>
+      <div className="Splash">
+        <div>
+          WELCOME
+        </div>
+        <div className="bla1">
+          TO
+        </div>
+        <div className="bla2">
+          CLASSIC ARMORY
+        </div>
       </div>
     </div>
+
 
   )
 }
