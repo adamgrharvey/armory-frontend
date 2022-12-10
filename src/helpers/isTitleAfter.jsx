7d18c98055e9a72fa50fccee491de1b8ad9c687e
adamgrharvey <adamgrharvey@gmail.com>
2022-12-09 16:07:17 -0800

    null title fix

--- src/helpers/isTitleAfter.jsx
+++ src/helpers/isTitleAfter.jsx
@@ -1,5 +1,8 @@
 
  export default function isTitleAfter(title) {
+  if (title === null) {
+    return false;
+  }
   let isAfter = false;
   if (title.slice(0,2) === 'of' || title.slice(0,3) === 'the' || title.slice(0,2) === ', ') {
     isAfter = true;
